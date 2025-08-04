c5330b585a659134ef8c36b95762a3ecd34e8df2
Genstark <gautamy499@gmail.com>
2025-08-04 20:22:10 +0530

    init e-learning project

---
+++ src/views/about.vue
@@ -0,0 +1,6 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+	<div class="about">
+		<h6>This is an about page</h6>
+	</div>
+</template>
---
+++ src/views/index.vue
@@ -0,0 +1,22 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+
+<template>
+	<div class="home">
+		<HelloWorld />
+		<p @click="change">change</p>
+		<button class="w-4">click</button>
+	</div>
+</template>
+
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue';
+import { useRouter } from 'vue-router';
+
+const route = useRouter();
+
+function change (event) {
+	event.preventDefault();
+	console.log('Change button clicked');
+	route.push('/about');
+}
+</script>
